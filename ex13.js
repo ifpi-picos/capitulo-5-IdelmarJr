b4d9505ba0dd86ff891c8f2012e7dc7dedda0900
Alex.lexi <capic.2024118tads0015@aluno.ifpi.edu.br>
2024-12-19 18:47:47 -0300

    alteração

--- ex13.js
+++ ex13.js
@@ -1,14 +1,15 @@
-let n = Math.floor(Math.ramdom() * 101);
+let n = Math.floor(Math.random() * 101);
 let num = 0;
-let cont = 0;
-while (cont < 10){
-    cont++
+
+for (let cont = 0; cont < 10; cont++) {
+
     num = parseInt(prompt('Digite um numero: '));
-    if(n === num){
+    if (n === num) {
         console.log('Parabens você acertou!');
         console.log(n);
+        break;
     } else {
-        if (num > n){
+        if (num > n) {
             console.log('maior do que o numero secreto');
         } else {
             console.log('menor do que o numero secreto');
